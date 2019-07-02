34b48126d18c212fd458a29afe021e4f34bd2704
cuervo96 <francomayo96@gmail.com>
2019-07-02 14:02:34 -0300

    Add files via upload

--- Energia.py
+++ Energia.py
@@ -7,13 +7,25 @@ Ecin = data[:,0]
 Epot = data[:,1]
 
 Etot = np.zeros(len(Ecin))
+T = np.zeros(len(Ecin))
 for i in range(0,len(Ecin)):
-	 Etot[i] = Ecin[i] + Epot[i] 
-
+	Etot[i] = Ecin[i] + Epot[i] 
+	T[i] = Ecin[i] * 2/3	
 plt.figure()
-plt.plot(Ecin[0:8000],'.')
-plt.plot(Epot[0:8000],'.')
-plt.plot(Etot[0:8000],'.')
+plt.subplot(221)
+plt.plot(Etot,'r.')
 plt.grid()
-plt.legend(["Ecin","Epot","Etot"])
+plt.ylabel("Etot")
+plt.subplot(222)
+plt.plot(T,'y.')
+plt.grid()
+plt.ylabel("Temperatura")
+plt.subplot(223)
+plt.plot(Ecin,'b.')
+plt.grid()
+plt.ylabel("Ecin")
+plt.subplot(224)
+plt.plot(Epot,'m.')
+plt.ylabel("Epot")
+plt.grid()
 plt.show()
